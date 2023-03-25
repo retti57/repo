10c44772883b3dcf1a47bd65cf5a196629edf019
Czaja <dczajka1@gmail.com>
2023-03-25 13:10:18 +0100

    Changed: win condition, applied most of suggestions from PR

--- projekty/tic_tac_toe_nowe_funkcje.py
+++ projekty/tic_tac_toe_nowe_funkcje.py
@@ -1,8 +1,22 @@
+class Player:
+    def __init__(self, marker: str):
+        self.marker = marker
+        self.player_set = set()
+
+    def __str__(self):
+        return f'Player {self.marker}'
+
+    def place(self):
+        place_to_choose = input('Select number 1-9 to place marker there: ')
+        if int(place_to_choose) in range(1, 10):
+            return int(place_to_choose)
+
+
 class Board:
 
-    def __init__(self):
-        # tworzenie pustej tablicy
-        self.board = [' ' for _ in range(10)]
+    board = ['-' for _ in range(10)]
+    win_set = [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
+    used_position_set = set()
 
     def __str__(self):
         return (
@@ -15,51 +29,45 @@ class Board:
 
     def add_to_board(self, place: int, marker: str, player_set: set):
         if place in player_set:
-            raise ValueError('Error in Board class add_to_board. Wrong number')
-        elif place not in player_set:
+            print('This place has been used before. Choose other number: 1-9')
+        else:
             self.board.pop(place)
             self.board.insert(place, marker)
 
-
-class Player:
-    def __init__(self, marker):
-        self.marker = marker
-        self.player_set = set()
-
-    def __str__(self):
-        return f'Player {self.marker}'
-
-    def place(self):
-        place_to_choose = int(input('Select number 1-9 to place marker there: '))
-        if place_to_choose in range(1, 10):
-            return place_to_choose
+    def win_check(self, player: Player):
+        for w_set in self.win_set:
+            if player.player_set.intersection(w_set) in self.win_set:
+                print(f'Player {player.marker} wins the game !')
+                return True
         else:
-            raise ValueError
+            return False
 
 
-def win_check(player_set, player):
-    win_set = [{1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7}]
-    for w_set in win_set:
-        if player_set.intersection(w_set) in win_set:
-            print(f'Player {player.marker} wins the game !')
-            return 'Finished Game'
-
-
-def round_flow(player, board, used_position_set):
+def round_flow(player: Player, board: Board, used_position_set: set):
     try:
         print(f'{player} turn')
         player_place = player.place()
-        if player_place in used_position_set:
-            raise ValueError
-        else:
+        while player_place not in range(1, 10):
+            print('--Wrong number--')
+            player_place = player.place()
+
+        if player_place not in used_position_set:
             board.add_to_board(player_place, player.marker, used_position_set)
             used_position_set.add(player_place)
             player.player_set.add(player_place)
+
             return 'Done'
+        else:
+            print('-Choose unoccupied place-')
     except ValueError:
-        print('Choose correct number')
+        print('! ! The integer seems to be not ! !')
     finally:
         print(board)
+        if board.win_check(player):
+            exit()
+        elif board.win_check(player) is False and len(used_position_set) == 9:
+            print("++++ REMIS SREMIS ++++")
+            exit()
 
 
 def player_one_marker():
@@ -71,14 +79,11 @@ def player_one_marker():
 
 
 board = Board()
-#      WYBÓR GRACZY
-
 print('Tic Tac Toe')
+
 marker = player_one_marker()
-used_position_set = set()
 player_one = Player(marker)
-
-
+#      WYBÓR GRACZY
 if player_one.marker == 'X':
     player_two = Player('O')
 
@@ -87,29 +92,19 @@ else:
 
 #      GAMEFLOW
 
+
 print(board)
-while len(used_position_set) != 9:
+while len(board.used_position_set) != 9:
     player_one_turn = True
     while player_one_turn:
-        if round_flow(player_one, board, used_position_set) == 'Done':
+        if round_flow(player_one, board, board.used_position_set) == 'Done':
             player_one_turn = False
-        elif ValueError:
-            continue
         else:
-            player_one_turn = False
-
-    if win_check(player_one.player_set, player_one) == 'Finished Game':
-        break
+            player_one_turn = True
 
     player_two_turn = True
     while player_two_turn:
-        if round_flow(player_two, board, used_position_set) == 'Done':
+        if round_flow(player_two, board, board.used_position_set) == 'Done':
             player_two_turn = False
-        elif ValueError:
-            continue
         else:
-            player_two_turn = False
-    if win_check(player_two.player_set, player_two) == 'Finished Game':
-        break
-
-#### warunek braku wygranej
+            player_two_turn = True
